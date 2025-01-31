80802e939e19ed0b907961bf0d2bf7e5a9851bf4
TAhmed-fra <tanvir.ahmed@stud.fra-uas.de>
2025-01-31 17:31:05 +0100

    Adding new functions to ReadData

--- source/Samples/NeocortexApiSamplePerformance/ReadData.cs
+++ source/Samples/NeocortexApiSamplePerformance/ReadData.cs
@@ -11,4 +11,17 @@ namespace NeocortexApiSamplePerformance
     public class ReadData
     {
 
+        public static List<MultiSequenceInput> MultiSequenceExcelInput(string filePath)
+        {
+            var inputs = new List<MultiSequenceInput>();
+
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var worksheet = package.Workbook.Worksheets[0]; // Assuming data is in the first sheet
+                int rowCount = worksheet.Dimension.Rows;
+
+                for (int row = 2; row <= rowCount; row++) // Assuming first row contains headers
+                {
+                    var input = new MultiSequenceInput
+                    {
         
